92be5e65d58b0b0c4eb6c2d761fbf94339d12556
BuildTools <unconfigured@null.spigotmc.org>
2020-04-29 02:28:43 +0100

    added more test methods

--- restaurant/src/main/java/org/academiadecodigo/lockdown/controller/TestController.java
+++ restaurant/src/main/java/org/academiadecodigo/lockdown/controller/TestController.java
@@ -8,9 +8,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class TestController {
 
-    @RequestMapping(method = RequestMethod.GET , path = "/")
+    @RequestMapping(method = RequestMethod.GET , path = "/list")
     public String serveTestPage(){
         return "restaurantList";
+    }
+
+    @RequestMapping(method = RequestMethod.GET , path = "/page")
+    public String serveTestPageTwo(){
+        return "restaurantPage";
+    }
+
+    @RequestMapping(method = RequestMethod.GET , path = "/register")
+    public String serveTestPageThree(){
+        return "restaurantPage";
     }
 
 }
